71031eef92a5032c9844677fead309f1de5b7ba8
San <yan_san_din@mail.ru>
2023-01-23 23:34:50 +0300

    first commit

---
+++ 21/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Поиск расстояния между 2 точками");
+
+
+Console.WriteLine("Введите координату xA");
+if (!double.TryParse(Console.ReadLine(), out double xA))
+{
+    Console.WriteLine("Введена неверная координата");
+    return;
+}
+
+Console.WriteLine("Введите координату xB");
+if (!double.TryParse(Console.ReadLine(), out double xB))
+{
+    Console.WriteLine("Введена неверная координата");
+    return;
+}
+
+Console.WriteLine("Введите координату yA");
+if (!double.TryParse(Console.ReadLine(), out double yA))
+{
+    Console.WriteLine("Введена неверная координата");
+    return;
+}
+
+Console.WriteLine("Введите координату yB");
+if (!double.TryParse(Console.ReadLine(), out double yB))
+{
+    Console.WriteLine("Введена неверная координата");
+    return;
+}
+
+Console.WriteLine("Введите координату zA");
+if (!double.TryParse(Console.ReadLine(), out double zA))
+{
+    Console.WriteLine("Введена неверная координата");
+    return;
+}
+
+Console.WriteLine("Введите координату zB");
+if (!double.TryParse(Console.ReadLine(), out double zB))
+{
+    Console.WriteLine("Введена неверная координата");
+    return;
+}
+
+
+double distance = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2) + Math.Pow(zB - zA, 2));
+
+Console.WriteLine($"Расстояние между точками - {distance}");
