05bdec12822edfceb0638f7b620d01538e3a2235
San <yan_san_din@mail.ru>
2023-01-23 23:43:34 +0300

    first commit

---
+++ 23/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Таблица кубов от 1 до N");
+
+int i = 1;
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 1)
+{
+    Console.WriteLine("Число должно быть > 0");
+    return;
+}
+
+Console.WriteLine("Таблица кубов: ");
+
+while (i < N + 1)
+{
+    Console.WriteLine(i * i * i);
+    i++;
+}
+
